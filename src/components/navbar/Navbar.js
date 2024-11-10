2581134b8ea2226f0facd78568f7f5511d9e0448
Raj-Timalsina <rajtimalsina2062@gmail.com>
2024-11-10 20:41:43 +0545

    Managed inital files and added a simple img

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import counterImage from "../../images/Navbar.png";
+import "./Navbar.css";
+function Navbar() {
+  return <img src={counterImage} alt="Navbar" />;
+}
+
+export default Navbar;
